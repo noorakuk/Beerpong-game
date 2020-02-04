30b8c500109b6649c13de44d6481cddf917fa336
Noora Kukkonen <noora.kukkonen@student.tuni.fi>
2020-02-04 14:04:55 +0200

    Main page is ready (not styles)

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // Page to create the server and initalize it
+const path = require('path');
+const express = require('express');
+const app = express();
+const port = 3000;
+
+
+app.listen(port, (err) => {
+    if (err) {
+        console.log(err);
+    }
+    console.log("Server is listenin on port " + port); 
+});
+
+// API!
+app.get("/", (req, res) => {
+    res.sendFile(path.join(__dirname + '/views/main-page.html'));
+})
